aa2e30653f3e2178b2ed354806f7efe6dfe0f0b8
oljar <oljar@tlen.pl>
2025-12-28 20:21:44 +0100

    obsługa przycisku ;RelayCommand;CommunityToolkit.Mvvm.ComponentModel

--- KoloryMaui/ModelWidoku/KolorMW.cs
+++ KoloryMaui/ModelWidoku/KolorMW.cs
@@ -1,98 +1,133 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Graphics;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Maui.Graphics;
+using System.Xml.Linq;
 
 
-namespace KoloryMaui.ModelWidoku
+
+namespace KoloryMaui.ModelWidoku;
+
+
+ partial class KolorMW : ObservableObject
 {
-    public class KolorMW : ObservedObject
-    {
-        public  Model.Kolor model = Model.Ustawienia.Czytaj();
+    public  Model.Kolor model = Model.Ustawienia.Czytaj();
 
-        #region Właściwości
-        public double R
-        {   
-            get => model.R;
-            set
-            {
-                if (model.R != value)
-                {
-                    model.R = value;
-                    OnPropertyChanged(nameof(R));
-                    OnPropertyChanged(nameof(SolidColeur));
-                }
-            }
-        }
-        public double G
+    #region Właściwości
+
+    
+    public double R
+    {   
+        get => model.R;
+        set
         {
-            get => model.G;
-            set
+            if (model.R != value)
             {
-                if (model.G != value)
-                {
-                    model.G = value;
-                    OnPropertyChanged(nameof(G));
-                    OnPropertyChanged(nameof(SolidColeur));
-                }
-            }
-        }
-         
-        public double B
-
-
-        {
-         
-
-            get => model.B;
-            set
-            {
-                if (model.B != value)
-                {
-                    model.B = value;
-                    OnPropertyChanged(nameof(B));
-                    OnPropertyChanged(nameof(SolidColeur));
-
-                }
-            }
-        }
-
-        // zmienia kolor rectangle na podstawie wartości RGB
-
-        public SolidColorBrush SolidColeur
-{
-            get
-            {
-                return new SolidColorBrush(model.Coleur);
-            }
-            set
-            {
-                PodajNaKoncole();
-                
-               
-                {
-                    model.Coleur = value.Color;
-                }
+                model.R = value;
+                OnPropertyChanged(nameof(R));
                 OnPropertyChanged(nameof(SolidColeur));
-                PodajNaKoncole();
             }
         }
+    }
+    public double G
+    {
+        get => model.G;
+        set
+        {
+            if (model.G != value)
+            {
+                model.G = value;
+                OnPropertyChanged(nameof(G));
+                OnPropertyChanged(nameof(SolidColeur));
+            }
+        }
+    }
+     
+    public double B
+
+
+    {
+     
+
+        get => model.B;
+        set
+        {
+            if (model.B != value)
+            {
+                model.B = value;
+                OnPropertyChanged(nameof(B));
+                OnPropertyChanged(nameof(SolidColeur));
+
+            }
+        }
+    }
+
+    // zmienia kolor rectangle na podstawie wartości RGB
+
+    public SolidColorBrush SolidColeur
+{
+        get
+        {
+            return new SolidColorBrush(model.Coleur);
+        }
+        set
+        {
+            PodajNaKoncole();
+            
+           
+            {
+                model.Coleur = value.Color;
+            }
+            OnPropertyChanged(nameof(SolidColeur));
+            PodajNaKoncole();
+        }
+    }
+    
+    public void PodajNaKoncole()
+            {
+                Debug.WriteLine($"R: {R}, G: {G}, B: {B}, Coleur: {model.Coleur}");
+            }
+    #endregion
+
+
+
+
+private readonly IRelayCommand _drukCommand;
+
+public IRelayCommand DrukCommand
+{
+    get => _drukCommand;
+}
+
+public KolorMW()
+{
+    this._drukCommand = new RelayCommand(Druk,CanDruk);
+}
+
+
+    private void Druk()
+    {
+            Debug.WriteLine("Naciśnięto przycisk Druk");
+       
+            this.R = 0;
+            this.G = 255;
+            this.B = 0;
+    }
+
+
+    private bool CanDruk()
+    {
         
-        public void PodajNaKoncole()
-                {
-                    Debug.WriteLine($"R: {R}, G: {G}, B: {B}, Coleur: {model.Coleur}");
-                }
-                    #endregion
+            return true;
+        
+    }
 
+ }
 
-
-
-
-
-            }
-        }
-
+    
