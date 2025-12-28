3863df6682bbbf78dd4b2ea6bbcc7c5a1ae049f0
oljar <oljar@tlen.pl>
2025-12-28 10:05:39 +0100

    Multibinding

--- KoloryMaui/Konwertery.cs
+++ KoloryMaui/Konwertery.cs
@@ -9,7 +9,7 @@ namespace KoloryMaui
 
         public static string ToByteString(double value)
         {
-            return Math.Round(255*value).ToString();
+            return Math.Round(255 * value).ToString();
         }
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
@@ -19,7 +19,29 @@ namespace KoloryMaui
 
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-           throw new NotImplementedException();
+            throw new NotImplementedException();
+        }
+    }
+
+
+    internal class RGBToBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (values.Contains(null)) return Brush.Black;
+            double r = (double)values[0];
+            double g = (double)values[1];
+            double b = (double)values[2];
+            return new SolidColorBrush(Color.FromRgb(r, g, b));
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
+
+
+
